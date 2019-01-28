23d37b0586b1a65c89329119b66bcf0bfb3e027f
egor-xyz <i@egor.xyz>
2019-01-28 13:26:25 +0200

    Updates

---
+++ precache-manifest.3db7caafb7095c3669c35461ecc7e33f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b38413bcd0c1fefab6d",
+    "url": "/pixi/static/css/main.b62fc5ac.chunk.css"
+  },
+  {
+    "revision": "1b38413bcd0c1fefab6d",
+    "url": "/pixi/static/js/main.1b38413b.chunk.js"
+  },
+  {
+    "revision": "de10d8e570fa2d673f86",
+    "url": "/pixi/static/css/1.569ade07.chunk.css"
+  },
+  {
+    "revision": "de10d8e570fa2d673f86",
+    "url": "/pixi/static/js/1.de10d8e5.chunk.js"
+  },
+  {
+    "revision": "4741090a6640d4503ca5",
+    "url": "/pixi/static/js/runtime~main.4741090a.js"
+  },
+  {
+    "revision": "eca94821a43415c3b44d018d8d2bc6fe",
+    "url": "/pixi/index.html"
+  }
+];
